4a5d53a7c7396e97961ca63b56d85dae3804020e
paperparrot5010 <me4030290@gmail.com>
2025-08-25 08:32:29 +0300

    restart every thing

--- World/cookies.gd
+++ World/cookies.gd
@@ -1,13 +1,11 @@
 extends Area2D
 
 
-signal collected(cookie_node)
+
 
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("PLAYER"):
-		emit_signal("collected",self)
-		queue_free()
+	pass
 		
 		
